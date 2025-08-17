ce61bbe6104d2c0a6df3c010954e4027012d7e92
raj-24 <nanivalluru2707@gmail.com>
2025-08-17 16:34:36 +0530

    second commit

--- src/main/java/com/example/student/StudentBatchApplication.java
+++ src/main/java/com/example/student/StudentBatchApplication.java
@@ -10,6 +10,7 @@ public class StudentBatchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentBatchApplication.class, args);
+		System.out.println("Application is Running Successfully......");
 	}
 
 }
